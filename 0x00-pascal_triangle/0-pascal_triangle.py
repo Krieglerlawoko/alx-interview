655855435981f530a3bc4ed568d21ed69aab8ca7
Krieglerlawoko <Krieglerlawokojudge@gmail.com>
2024-06-05 16:42:26 +0000

    new

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,18 @@
+#!/usr/bin/python3
+
 def pascal_triangle(n):
+    """
+    Generate Pascal's triangle up to the nth row.
+    """
     if n <= 0:
         return []
 
     triangle = [[1]]
-
     for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]  # First element of the row is always 1
+        row = [1]
         for j in range(1, i):
-            new_row.append(prev_row[j - 1] + prev_row[j])
-        new_row.append(1)  # Last element of the row is always 1
-        triangle.append(new_row)
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
 
     return triangle
