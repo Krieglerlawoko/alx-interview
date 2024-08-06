e35644e6732a41a9d0354dc03cc9dcf2faef09c1
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2024-08-06 11:27:25 -0700

    n

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,40 +4,47 @@ Prime game module.
 """
 
 
-def is_winner(x, nums):
-    """
-    Determines the winner of a prime game session with `x` rounds.
+def sieve(n):
+    """Generate a list of primes up to n using the Sieve of Eratosthenes."""
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+    for start in range(2, int(n**0.5) + 1):
+        if is_prime[start]:
+            for multiple in range(start*start, n + 1, start):
+                is_prime[multiple] = False
+    return [num for num, prime in enumerate(is_prime) if prime]
 
-    Parameters:
-    x (int): The number of rounds.
-    nums (list): A list of integers representing the end number for each round.
 
-    Returns:
-    str: The winner of the game, either
-    "Maria" or "Ben". Returns None if it's a tie.
-    """
-    if x < 1 or not nums:
+def isWinner(x, nums):
+    if not nums or x <= 0:
         return None
+    max_num = max(nums)
+    primes = sieve(max_num)
+    prime_set = set(primes)
 
-    marias_wins, bens_wins = 0, 0
+    def game_outcome(n):
+        # Tracks moves made in a set
+        moves = [0] * (n + 1)
+        for prime in primes:
+            if prime > n:
+                break
+            for multiple in range(prime, n + 1, prime):
+                moves[multiple] += 1
+        return moves
 
-    # Generate primes with a limit of the maximum number in nums
-    n = max(nums)
-    primes = [True] * (n + 1)
-    primes[0] = primes[1] = False
-
-    for i in range(2, int(n ** 0.5) + 1):
-        if primes[i]:
-            for j in range(i * i, n + 1, i):
-                primes[j] = False
-
-    # Filter the number of primes less than or equal
-    for num in nums:
-        primes_count = sum(primes[0:num + 1])
-        bens_wins += primes_count % 2 == 0
-        marias_wins += primes_count % 2 == 1
-
-    if marias_wins == bens_wins:
+    # Determines the winner for each round
+    maria_wins = 0
+    ben_wins = 0
+    for n in nums:
+        moves = game_outcome(n)
+        # If the number of prime moves is even, Ben wins (because Maria starts)
+        if sum(moves) % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
         return None
-
-    return 'Maria' if marias_wins > bens_wins else 'Ben'
